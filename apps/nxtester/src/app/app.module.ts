3b9f00fe789c7c31d73a83d45870839dde43e6d0
joshuabrink1 <u19185678@tuks.co.za>
2022-04-14 17:56:03 +0200

    Added hello component

--- apps/nxtester/src/app/app.module.ts
+++ apps/nxtester/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HelloComponent } from './hello/hello.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, HelloComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
